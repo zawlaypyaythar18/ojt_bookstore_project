faff4c542c32871ea94c52a5956694c4dd8bc66d
Zaw Lay Pyay Thar <zawlaytest3@gmail.com>
2022-12-23 07:31:40 +0630

    zaw - user and book list doc

--- backend/src/main/java/com/OJTProject/bookstore/controller/HomeController.java
+++ backend/src/main/java/com/OJTProject/bookstore/controller/HomeController.java
@@ -1,15 +1,10 @@
 package com.OJTProject.bookstore.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HomeController {
 	
-	@GetMapping("/")
-	public ResponseEntity<String> home() {
-		return ResponseEntity.ok("Hello World");
-	}
-
 }
